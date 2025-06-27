8cd78f0c943adad63ab38be05b090f8bb7dba58e
C2gl <97646342+C2gl@users.noreply.github.com>
2025-06-27 15:52:34 +0200

    array structure

--- auto_git_pull.bat
+++ auto_git_pull.bat
@@ -1,3 +1,5 @@
+@echo off
+setlocal enabledelayedexpansion
 
 set "waittime=10"
 
@@ -7,195 +9,18 @@ set "path3=C:\Users\lieve\Documents\GitHub\csm"
 
 set "count=0"
 
-
 FOR /L %%A in (1,1,100) DO (
-    echo %%A
-    echo for loop test %%A
-    cd "%path1%"
+    set /a index=%%A %% 3
+    if !index! == 1 set "currentPath=!path1!"
+    if !index! == 2 set "currentPath=!path2!"
+    if !index! == 0 set "currentPath=!path3!"
+
+    echo Iteration %%A
+    echo Current path: !currentPath!
+    cd /d "!currentPath!"
     git pull
-    count = count +1
-    timeout /t 50
+    set /a count+=1
+    timeout /t %waittime% >nul
 )
 
-cd blink
-git pull
-cd ..
-timeout /t %waittime%
-
-cd core
-git pull
-cd ..
-timeout /t %waittime%
-
-cd csm
-git pull
-cd ..
-timeout /t %waittime%
-
-cd ferrishare
-git pull
-cd ..
-timeout /t %waittime%
-
-cd firefly-iii
-git pull
-cd ..
-timeout /t %waittime%
-
-cd Fladder
-git pull
-cd ..
-timeout /t %waittime%
-
-cd home_server
-git pull
-cd ..
-timeout /t %waittime%
-
-cd immich
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyfin
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyfin-media-player
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyfin-plugin-streamyfin
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyfin-web
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyfin-webos
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellystat
-git pull
-cd ..
-timeout /t %waittime%
-
-cd ladybird
-git pull
-cd ..
-timeout /t %waittime%
-
-cd ProxmoxVE
-git pull
-cd ..
-timeout /t %waittime%
-
-cd spksrc
-git pull
-cd ..
-timeout /t %waittime%
-
-cd streamyfin
-git pull
-cd ..
-timeout /t %waittime%
-
-cd streamystats
-git pull
-cd ..
-timeout /t %waittime%
-
-cd Swiftfin
-git pull
-cd ..
-timeout /t %waittime%
-
-cd syncthing
-git pull
-cd ..
-timeout /t %waittime%
-
-cd tailscale
-git pull
-cd ..
-timeout /t %waittime%
-
-cd tuwunel
-git pull
-cd ..
-timeout /t %waittime%
-
-cd zigbee2mqtt
-git pull
-cd ..
-timeout /t %waittime%
-
-cd grocy
-git pull
-cd ..
-timeout /t %waittime%
-
-cd paperless-ngx
-git pull
-cd ..
-timeout /t %waittime%
-
-cd manage-my-damn-life-nextjs
-git pull
-cd ..
-timeout /t %waittime%
-
-cd dockge
-git pull
-cd ..
-timeout /t %waittime%
-
-
-cd synapse
-git pull
-cd ..
-timeout /t %waittime%
-
-cd element-x-ios
-git pull
-cd ..
-timeout /t %waittime%
-
-cd nginx-proxy-manager
-git pull
-cd ..
-timeout /t %waittime%
-
-cd jellyseerr
-git pull
-cd ..
-timeout /t %waittime%
-
-cd filebrowser
-git pull
-cd ..
-timeout /t %waittime%
-
-cd paperless-ai
-git pull
-cd ..
-timeout /t %waittime%
-
-cd tududi
-git pull
-cd ..
-timeout /t %waittime%
-
-cd endurain
-git pull
-cd ..
-timeout /t %waittime%
-
-timeout /t -1
+echo Total pulls: %count%
