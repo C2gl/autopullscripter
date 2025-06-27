4591fd9f9754d5d09fe1a1d68049972bff4a24ca
C2gl <97646342+C2gl@users.noreply.github.com>
2025-06-27 16:36:03 +0200

    added documentation

--- auto_git_pull.bat
+++ auto_git_pull.bat
@@ -1,15 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: wait time between pulls in the loop
 set "waittime=10"
 
+:: defined paths to git repositories
 set "path1=C:\Users\lieve\Documents\GitHub\blink"
 set "path2=C:\Users\lieve\Documents\GitHub\core"
 set "path3=C:\Users\lieve\Documents\GitHub\csm"
 
+:: counter to tell how many pulls were done
+:: the pathcounter variable is used to cycle through only the amount of paths defined
 set "count=0"
 set "pathcount=3"
 
+
+:: working for loop
 FOR /L %%A in (1,1,%pathcount%) DO (
     set /a index=%%A %% 3
     if !index! == 1 set "currentPath=!path1!"
