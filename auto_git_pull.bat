597b4b86ee74727902e8a9f1c4150e5bd0f535e2
C2gl <97646342+C2gl@users.noreply.github.com>
2025-06-27 16:59:49 +0200

    working loop, having to add all others

--- auto_git_pull.bat
+++ auto_git_pull.bat
@@ -8,19 +8,23 @@ set "waittime=10"
 set "path1=C:\Users\lieve\Documents\GitHub\blink"
 set "path2=C:\Users\lieve\Documents\GitHub\core"
 set "path3=C:\Users\lieve\Documents\GitHub\csm"
+set "path4=C:\Users\lieve\Documents\GitHub\dockge"
+set "path5=C:\Users\lieve\Documents\GitHub\element-x-ios"
 
 :: counter to tell how many pulls were done
 :: the pathcounter variable is used to cycle through only the amount of paths defined
 set "count=0"
-set "pathcount=3"
+set "pathcount=5"
 
 
 :: working for loop
 FOR /L %%A in (1,1,%pathcount%) DO (
-    set /a index=%%A %% 3
-    if !index! == 1 set "currentPath=!path1!"
-    if !index! == 2 set "currentPath=!path2!"
-    if !index! == 0 set "currentPath=!path3!"
+    set /a index=%%A %% %pathcount%
+    if !count! == 0 set "currentPath=!path1!"
+    if !count! == 1 set "currentPath=!path2!"
+    if !count! == 2 set "currentPath=!path3!"
+    if !count! == 3 set "currentPath=!path4!"
+    if !count! == 4 set "currentPath=!path5!"
 
     echo Iteration %%A
     echo Current path: !currentPath!
