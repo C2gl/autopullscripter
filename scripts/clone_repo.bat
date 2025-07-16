ef78f1d01dc232dbe3cddb292803323549b6ef58
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-16 15:23:06 +0200

    comment added

--- scripts/clone_repo.bat
+++ scripts/clone_repo.bat
@@ -2,7 +2,7 @@ setlocal enabledelayedexpansion
 
 @ echo off
 
-:: script to add a new repository URL to the repos.txt file
+:: script to add a new repository URL to the variables and cloning it
 echo what is the URL of the repository you want to add?
 set /p "repo_url=Enter repository URL: "
 set /p "path=Enter the local path where you want to clone the repository: "
