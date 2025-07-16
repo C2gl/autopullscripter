7bc20ac78985e8a8ec34b7cadaeb62daf6032997
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-16 15:26:34 +0200

    adding new repo to repos.txt

--- scripts/clone_repo.bat
+++ scripts/clone_repo.bat
@@ -10,3 +10,15 @@ set /p "path=Enter the local path where you want to clone the repository: "
 :: cloning the new repository
 cd /d "%path%"
 git clone "%repo_url%"
+
+
+:: extract repo name from URL (remove .git if present)
+for %%A in ("%repo_url%") do (
+    set "repo_name=%%~nA"
+)
+if /i "!repo_url:~-4!"==".git" (
+    set "repo_name=!repo_name:~0,-4!"
+)
+
+:: add the full path to repos.txt
+echo %path%\!repo_name!>>"..\..\repos.txt"
