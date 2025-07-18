22ca9d9cbdc0ad032262050d687347f7aefe03e1
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-19 00:29:06 +0200

    creation of the logfile

--- scripts/log.bat
+++ scripts/log.bat
@@ -2,3 +2,22 @@
 setlocal enabledelayedexpansion
 :: file meant to be run by run.bat, will check if there is a log folder and create it if not
 :: then it will log the current date and time to a log file 
+
+set "logfile=log.txt"
+if not exist "log" (
+    mkdir "log"
+    echo Log folder created."
+)
+
+:: Get date and time, remove invalid filename characters
+set "dt=%date%_%time%"
+set "dt=%dt:/=-%"
+set "dt=%dt::=-%"
+set "dt=%dt: =_%"
+set "dt=%dt:.=-%"
+
+:: Create unique logfile name
+set "logfile=log_%dt%.txt"
+
+:: Write to the unique logfile
+echo %date% %time% >> "log\%logfile%"
